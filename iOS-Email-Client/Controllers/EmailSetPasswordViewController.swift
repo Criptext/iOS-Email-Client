b61b0843e2c049c839d88a8722027247a18eecad
Pedro Iñiguez <pedro_aim@hotmail.com>
2018-06-07 17:24:32 -0500

    set email password view

---
+++ iOS-Email-Client/Controllers/EmailSetPasswordViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EmailSetPasswordViewController.swift
+//  iOS-Email-Client
+//
+//  Created by Pedro Aim on 6/7/18.
+//  Copyright © 2018 Criptext Inc. All rights reserved.
+//
+
+import Foundation
+
+class EmailSetPasswordViewController: BaseUIPopover {
+    
+    init(){
+        super.init("EmailSetPasswordUIPopover")
+    }
+    
+    override func viewDidLoad() {
+
+    }
+}
