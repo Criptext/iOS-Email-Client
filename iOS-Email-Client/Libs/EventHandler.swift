a1767a14325d803b594465ed6abb58898f4f30b3
Pedro Iñiguez <pedro_aim@hotmail.com>
2018-06-22 12:30:01 -0500

    sent email is also received by my other devices (#50)
    * sent email is also received by my other devices
    
    * domains replaced by constant

--- iOS-Email-Client/Libs/EventHandler.swift
+++ iOS-Email-Client/Libs/EventHandler.swift
@@ -124,7 +124,7 @@ class EventHandler {
         let localDate = dateFormatter.date(from: date) ?? Date()
         
         if let email = DBManager.getMailByKey(key: metadataKey.description) {
-            if(email.labels.count == 1 && email.labels.first!.id == SystemLabel.sent.id){
+            if(isMeARecipient(email: email)){
                 DBManager.addRemoveLabelsFromEmail(email, addedLabelIds: [SystemLabel.inbox.id], removedLabelIds: [])
                 finishCallback(true, email)
                 return
@@ -163,13 +163,19 @@ class EventHandler {
                 return
             }
             email.preview = String(email.content.removeHtmlTags().prefix(100))
-            email.labels.append(DBManager.getLabel(SystemLabel.inbox.id)!)
             DBManager.store(email)
             
             ContactManager.parseEmailContacts(from, email: email, type: .from)
             ContactManager.parseEmailContacts(to, email: email, type: .to)
             ContactManager.parseEmailContacts(cc, email: email, type: .cc)
             ContactManager.parseEmailContacts(bcc, email: email, type: .bcc)
+            
+            if(self.isFromMe(email: email)){
+                DBManager.addRemoveLabelsFromEmail(email, addedLabelIds: [SystemLabel.sent.id], removedLabelIds: [])
+            }
+            if(self.isMeARecipient(email: email)){
+                DBManager.addRemoveLabelsFromEmail(email, addedLabelIds: [SystemLabel.inbox.id], removedLabelIds: [])
+            }
             finishCallback(true, email)
         }
     }
@@ -184,8 +190,8 @@ class EventHandler {
         open.fileId = fileId
         open.isNew = true
         open.date = Utils.getLocalDate(from: date)
-        guard !DBManager.feedExists(emailId: emailId, type: open.type, contactId: "\(from)@jigl.com"),
-            let contact = DBManager.getContact("\(from)@jigl.com"),
+        guard !DBManager.feedExists(emailId: emailId, type: open.type, contactId: "\(from)\(Constants.domain)"),
+            let contact = DBManager.getContact("\(from)\(Constants.domain)"),
             let email = DBManager.getMail(key: emailId) else {
             finishCallback(true, nil)
             return
@@ -208,6 +214,21 @@ class EventHandler {
         file.emailId = email.key
         DBManager.store(file)
         return file
+    }
+    
+    func isMeARecipient(email: Email) -> Bool {
+        let accountEmail = "\(myAccount.username)\(Constants.domain)"
+        let bccContacts = Array(email.getContacts(type: .bcc))
+        let ccContacts = Array(email.getContacts(type: .cc))
+        let toContacts = Array(email.getContacts(type: .to))
+        return bccContacts.contains(where: {$0.email == accountEmail})
+            || ccContacts.contains(where: {$0.email == accountEmail})
+            || toContacts.contains(where: {$0.email == accountEmail})
+    }
+    
+    func isFromMe(email: Email) -> Bool {
+        let accountEmail = "\(myAccount.username)\(Constants.domain)"
+        return accountEmail == email.fromContact.email
     }
 }
 
