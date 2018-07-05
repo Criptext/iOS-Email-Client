2a817486f35aa774753a05fac4d3dc5f30c03e48
Pedro Iñiguez <pedro_aim@hotmail.com>
2018-07-05 09:37:39 -0500

    removing unused stuff

--- iOS-Email-Client/Managers/ContactUtils.swift
+++ iOS-Email-Client/Managers/ContactUtils.swift
@@ -12,49 +12,6 @@ import Contacts
 class ContactUtils {
     static let store = CNContactStore()
     
-    class func getContacts(completion: @escaping ((Bool, [Contact]?) -> Void)){
-        self.store.requestAccess(for: .contacts) { (granted, error) in
-            guard granted else {
-                completion(false, nil)
-                return
-            }
-            
-            let keysToFetch:[CNKeyDescriptor] = [CNContactFormatter.descriptorForRequiredKeys(for: .fullName), CNContactEmailAddressesKey as CNKeyDescriptor]
-            
-            let request = CNContactFetchRequest(keysToFetch: keysToFetch )
-            var cnContacts = [CNContact]()
-            
-            do {
-                try self.store.enumerateContacts(with: request){
-                    (contact, cursor) -> Void in
-                    cnContacts.append(contact)
-                }
-            } catch let error {
-                NSLog("Fetch contact error: \(error)")
-                
-            }
-            
-            var contacts = [Contact]()
-            
-            for contact in cnContacts {
-                let emails = contact.emailAddresses.map{ return String($0.value) }
-                
-                guard !emails.isEmpty, let fullname = CNContactFormatter.string(from: contact, style: .fullName) else {
-                    continue
-                }
-                
-                for email in emails {
-                    let contact = Contact()
-                    contact.displayName = fullname
-                    contact.email = email.replacingOccurrences(of: " ", with: "")
-                    contacts.append(contact)
-                }
-            }
-            
-            completion(true, contacts)
-        }
-    }
-    
     private class func parseContact(_ contactString: String) -> Contact {
         let splittedContact = contactString.split(separator: "<")
         guard splittedContact.count > 1 else {
