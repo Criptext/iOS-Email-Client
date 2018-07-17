a307c1d421e621a6dc7dbe0a667c5e9b03e32147
Pedro Iñiguez <pedro_aim@hotmail.com>
2018-07-17 10:31:07 -0500

    adding test to check for generated file

--- iOS-Email-Client/AsyncTasks/SenderLinkDeviceAsyncTask.swift
+++ iOS-Email-Client/AsyncTasks/SenderLinkDeviceAsyncTask.swift
@@ -14,15 +14,15 @@ class SenderLinkDeviceAsyncTask {
     
     let fileURL = CriptextFileManager.getURLForFile(name: "link-device-\(Date().timeIntervalSince1970)")
     
-    func start(completion: @escaping ((Error?, Any?) -> Void)){
+    func start(completion: @escaping ((Error?, URL?) -> Void)){
         try? FileManager.default.removeItem(at: fileURL)
         let queue = DispatchQueue(label: "com.email.senderlink", qos: .background, attributes: .concurrent)
         queue.async {
-            self.createDBFile()
+            self.createDBFile(completion: completion)
         }
     }
     
-    func createDBFile(){
+    func createDBFile(completion: @escaping ((Error?, URL?) -> Void)){
         let results = DBManager.retrieveWholeDB()
         let contacts = results["contacts"] as! Results<Contact>
         contacts.forEach { (contact) in
@@ -41,7 +41,6 @@ class SenderLinkDeviceAsyncTask {
                 guard let jsonString = Utils.convertToJSONString(dictionary: emailLabelDictionary) else {
                     return
                 }
-                print(jsonString)
                 writeRowToFile(jsonRow: jsonString)
             })
         }
@@ -51,24 +50,31 @@ class SenderLinkDeviceAsyncTask {
         }
         let files = results["files"] as! Results<File>
         files.forEach { (file) in
+            guard file != files.last else {
+                handleRow(file, appendNewLine: false)
+                return
+            }
             handleRow(file)
+        }
+        
+        DispatchQueue.main.async {
+            completion(nil, self.fileURL)
         }
     }
     
-    private func handleRow(_ row: Any){
+    private func handleRow(_ row: Any, appendNewLine: Bool = true){
         guard let item = row as? CustomDictionary else {
             return
         }
         guard let jsonString = Utils.convertToJSONString(dictionary: item.toDictionary()) else {
             return
         }
-        print(jsonString)
-        writeRowToFile(jsonRow: jsonString)
+        writeRowToFile(jsonRow: jsonString, appendNewLine: appendNewLine)
     }
     
-    private func writeRowToFile(jsonRow: String){
-        let rowData = "\(jsonRow)\n".data(using: .utf8)!
-        if FileManager.default.fileExists(atPath: fileURL.absoluteString) {
+    private func writeRowToFile(jsonRow: String, appendNewLine: Bool = true){
+        let rowData = "\(jsonRow)\(appendNewLine ? "\n" : "")".data(using: .utf8)!
+        if FileManager.default.fileExists(atPath: fileURL.path) {
             let fileHandle = try! FileHandle(forUpdating: fileURL)
             fileHandle.seekToEndOfFile()
             fileHandle.write(rowData)
