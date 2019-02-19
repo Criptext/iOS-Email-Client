36f38f504999a6337eae1321ee885c31544f2023
Pedro Aim <pedro_aim@hotmail.com>
2019-02-19 14:35:16 -0500

    check if zip exists

--- scripts/firebase.sh
+++ scripts/firebase.sh
@@ -1,7 +1,8 @@
-if [ ! -d ./Firebase ]; then
+if [ ! -f ./Firebase/Firebase-5.4.1.zip ]; then
   echo "Downloading Firebase 5.4.1..."
   curl -O "https://cdn.criptext.com/ios/Firebase-5.4.1.zip"
   unzip Firebase-5.4.1.zip
+  cp ./Firebase-5.4.1.zip ./Firebase/
 else 
   echo "Firebase already exists!"
 fi
@@ -10,7 +11,7 @@ echo "Copying Files..."
 
 cp -a ./Firebase/Analytics/. ./
 cp -a ./Firebase/Messaging/. ./
-cp ./Firebase/Firebase.h ./iOS-Email-Client
-cp ./Firebase/module.modulemap ./iOS-Email-Client
+cp ./Firebase/Firebase.h ./iOS-Email-Client/
+cp ./Firebase/module.modulemap ./iOS-Email-Client/
 
 echo "Firebase Script Completed!"
