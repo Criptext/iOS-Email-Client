ecc2040ddbe7cb9df24b1f2de99946baa4a04aac
Pedro Aim <pedro_aim@hotmail.com>
2019-04-30 15:57:44 -0500

    resolves #398, resolves #399

--- iOS-Email-Client/Libs/Env.swift
+++ iOS-Email-Client/Libs/Env.swift
@@ -49,28 +49,28 @@ struct Env {
         guard !Env.isProduction else {
             return "wss://socket.criptext.com"
         }
-        return "wss://stagesocket.criptext.com"
+        return "wss://socket.criptext.com"
     }
     
     static var domain: String {
         guard !Env.isProduction else {
             return "@criptext.com"
         }
-        return "@jigl.com"
+        return "@criptext.com"
     }
     
     static var apiURL: String {
         guard !Env.isProduction else {
             return "https://api.criptext.com"
         }
-        return "https://stage.mail.criptext.com"
+        return "https://api.criptext.com"
     }
     
     static var transferURL: String {
         guard !Env.isProduction else {
             return "https://transfer.criptext.com"
         }
-        return "https://stagetransfer.criptext.com"
+        return "https://transfer.criptext.com"
     }
     
     static var language: String {
